820731bd388c2075522244dd69d74f62d6e3abe1
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-05-22 01:12:56 +0600

    Understanding Left and Right Joins

--- join.sql
+++ join.sql
@@ -1,2 +1,11 @@
 SELECT title, username FROM posts as p
     JOIN users as u ON p.user_id = u.id
+
+INSERT INTO posts(title, user_id) VALUES('This title 7', NULL)
+
+
+SELECT * FROM posts
+    LEFT JOIN users ON posts.user_id = users.id
+
+SELECT * FROM users
+    RIGHT JOIN posts ON users.id = posts.user_id
