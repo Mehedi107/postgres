7c72e0dc49e5be9ca32e9a912daa278a81b6c07a
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-05-25 19:48:33 +0600

    add first question answer to readme file

--- L2A2/PostgreSQL_Assignment.sql
+++ L2A2/PostgreSQL_Assignment.sql
@@ -48,32 +48,27 @@ select * from sightings
 -- ///////////////////////////////////////////////////////
 
 -- Problem: 1 
--- Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
 INSERT INTO rangers(name, region) 
     VALUES('Derek Fox', 'Coastal Plains')
 
 
 -- Problem: 2
---  Count unique species ever sighted.
-select common_name, count(*) as unique_species_count from species 
-    GROUP BY common_name
+SELECT count(DISTINCT species_id) AS unique_species_count
+FROM sightings
 
 
 -- Problem: 3 
--- Find all sightings where the location includes "Pass".
 select * from sightings
     WHERE location ILIKE '%pass%';
 
 
 -- Problem: 4
--- List each ranger's name and their total number of sightings.
 SELECT name, count(*) as total_sightings FROM rangers
     JOIN sightings ON rangers.ranger_id = sightings.ranger_id
     GROUP BY name
 
 
 -- Problem: 5
--- List species that have never been sighted.
 SELECT common_name FROM species
     LEFT JOIN sightings on species.species_id = sightings.species_id
     WHERE sightings.species_id IS NULL
@@ -81,7 +76,6 @@ SELECT common_name FROM species
 
 
 -- Problem: 6
---  Show the most recent 2 sightings.
 SELECT common_name, sighting_time, name FROM sightings
     JOIN species ON sightings.species_id = species.species_id
     JOIN rangers ON sightings.ranger_id = rangers.ranger_id
@@ -90,14 +84,12 @@ SELECT common_name, sighting_time, name FROM sightings
 
 
 -- Problem: 7
--- Update all species discovered before year 1800 to have status 'Historic'.
 UPDATE species
 SET conservation_status = 'Historic'
 WHERE discovery_date < '1800-01-01';
 
 
 -- Problem: 8
--- Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
 SELECT sighting_id,
     CASE
     WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
@@ -109,7 +101,6 @@ FROM sightings
 
 
 -- Problem: 9
--- Delete rangers who have never sighted any species
 DELETE from rangers r
 WHERE NOT EXISTS (
   SELECT 1
